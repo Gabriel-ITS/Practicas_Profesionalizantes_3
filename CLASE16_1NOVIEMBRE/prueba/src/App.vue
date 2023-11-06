2e1480d92d5292c87ef799f6258f948424de7f39
Gabriel-ITS <102498487+Gabriel-ITS@users.noreply.github.com>
2023-11-06 19:32:43 -0300

    clase 16

---
+++ CLASE16_1NOVIEMBRE/prueba/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Prueba</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link class="nav-kink" to="/">Home</router-link>
+          <router-link class="nav-kink" to="/about">About</router-link>
+          <router-link class="nav-kink" to="/clientes">Clientes</router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <div class="container mt-3">
+    <router-view/>
+  </div>
+</template>
---
+++ CLASE16_1NOVIEMBRE/prueba/src/views/Cliente.vue
@@ -0,0 +1,13 @@
+<template>
+    <FormCliente />
+</template>
+<script>
+/* eslint-disable */
+import FormCliente from "../components/FormCliente.vue";
+export default {
+    name: 'Cliente',
+    components:{
+        FormCliente
+    }
+}
+</script>
